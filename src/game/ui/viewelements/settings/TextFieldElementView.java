553686508fa67e7dfecbfaee8d9614cf9c04c286
simonediberardino <simonediberardino7@gmail.com>
2023-05-26 17:21:40 +0200

    - Added mouse hover color to info text field;

--- src/game/ui/viewelements/settings/TextFieldElementView.java
+++ src/game/ui/viewelements/settings/TextFieldElementView.java
@@ -2,40 +2,55 @@ package game.ui.viewelements.settings;
 
 import game.models.RunnablePar;
 import game.ui.helpers.UppercaseDocumentsFilter;
+import game.values.BomberColors;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
 import javax.swing.text.AbstractDocument;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 public class TextFieldElementView extends SettingsElementView{
     private final JLabel name;
     private final JTextField value;
-    private final RunnablePar callback;
+    private final Color defaultColor = Color.WHITE;
+    private final Color mouseHoverColor = BomberColors.RED;
 
     public TextFieldElementView(JPanel gridPanel, String title, String startText, RunnablePar callback) {
-        super(gridPanel);
-        // Creating two JLabels to display the title and value
-        name = new JLabel(title, SwingConstants.CENTER);
-        value = new JTextField( 20);
+        super(gridPanel); // Call the constructor of the superclass and pass gridPanel as a parameter.
+        name = new JLabel(title, SwingConstants.CENTER); // Create a new JLabel named "name" with the specified title and center alignment.
+        value = new JTextField(1);
 
-        this.callback = callback;
 
-        value.setOpaque(false);
-        value.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
+        value.setOpaque(false); // Set the opaque property of the "value" text field to false, allowing the background to be transparent.
+        value.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT); // Set the component orientation of the "value" text field to right-to-left.
         value.setText(startText.toUpperCase());
-        value.addCaretListener(e -> callback.execute(value.getText()));
-        ((AbstractDocument) value.getDocument()).setDocumentFilter(new UppercaseDocumentsFilter());
-        value.setBorder(BorderFactory.createEmptyBorder(0, 0, 0, 0));
 
-        // Setting foreground color of the labels to white
+        value.addCaretListener(e -> callback.execute(value.getText())); // Add a caret listener to the "value" text field that executes the "callback" function with the current text of the field when the caret position changes.
+        ((AbstractDocument) value.getDocument()).setDocumentFilter(new UppercaseDocumentsFilter()); // Set a document filter on the "value" text field to enforce uppercase text input.
+        value.setBorder(BorderFactory.createEmptyBorder());
+
+        // Adding mouse listeners to the textfield;
+        value.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                value.setForeground(mouseHoverColor); // Set the foreground color of the "value" text field to "mouseHoverColor" when the mouse enters.
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                value.setForeground(defaultColor); // Set the foreground color of the "value" text field to "defaultColor" when the mouse exits.
+            }
+        });
+
         name.setForeground(Color.WHITE);
         value.setForeground(Color.WHITE);
-        // Setting font for the labels
         name.setFont(new Font(Font.MONOSPACED, Font.BOLD, FONT_SIZE));
         value.setFont(new Font(Font.MONOSPACED, Font.BOLD, FONT_SIZE));
 
-        // Adding name and value labels to the SettingsElementView object
-        add(name);
-        add(value);
+        add(name); // Add the "name" label to the SettingsElementView object.
+        add(value); // Add the "value" text field to the SettingsElementView object.
     }
 }
