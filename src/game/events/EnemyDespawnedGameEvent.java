d671653814476e21a88c487e77d1ceb0a13261b3
simonediberardino <simonediberardino7@gmail.com>
2023-10-03 17:34:58 +0200

    - Refactoring; - Fix remote;

--- src/game/events/EnemyDespawnedGameEvent.java
+++ src/game/events/EnemyDespawnedGameEvent.java
@@ -5,7 +5,7 @@ import game.Bomberman;
 public class EnemyDespawnedGameEvent implements GameEvent {
     @Override
     public void invoke(Object arg) {
-        if(!Bomberman.getMatch().getGameState()) return;
+        if (!Bomberman.getMatch().getGameState()) return;
 
         System.out.println("KILLED ENEMY! " + Bomberman.getMatch().getEnemiesAlive());
         if(Bomberman.getMatch().getEnemiesAlive() == 0) {
