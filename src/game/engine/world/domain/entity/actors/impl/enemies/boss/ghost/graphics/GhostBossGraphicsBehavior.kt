44521a70222263900b8b00fd16d8f58545dd92cf
Simone Di Berardino <simonediberardino7@gmail.com>
2024-03-06 22:47:51 +0100

    WIP REFACTORING HIERARCHY ENTITY SYSTEM: Ghost Boss and Clown Boss

---
+++ src/game/engine/world/domain/entity/actors/impl/enemies/boss/ghost/graphics/GhostBossGraphicsBehavior.kt
@@ -0,0 +1,17 @@
+package game.engine.world.domain.entity.actors.impl.enemies.boss.ghost.graphics
+
+import game.engine.world.domain.entity.actors.impl.enemies.boss.base.Boss
+import game.engine.world.domain.entity.actors.impl.enemies.boss.base.graphics.BossEntityGraphicsBehavior
+import game.engine.world.domain.entity.actors.impl.enemies.boss.ghost.GhostBoss
+import game.utils.file_system.Paths
+
+class GhostBossGraphicsBehavior(override val entity: Boss) : BossEntityGraphicsBehavior(entity = entity) {
+    override fun getImageFromRageStatus(): String {
+        return java.lang.String.format(
+                GhostBoss.SKIN_PATH_TEMPLATE,
+                Paths.enemiesFolder,
+                entity.state.imageDirection.toString().lowercase(),
+                entity.state.currRageStatus
+        )
+    }
+}
